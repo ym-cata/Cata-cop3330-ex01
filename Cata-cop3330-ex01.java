27d0e18da20285969fdaed61bd8b88f3f0148ee5
ym-cata <89892057+ym-cata@users.noreply.github.com>
2021-09-13 22:08:32 -0400

    Add files via upload

---
+++ Cata-cop3330-ex01.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Yesenia Cata
+ */
+
+
+class Main {
+  public static void main(String[] args) {
+     var scanner = new java.util.Scanner(System.in);
+
+    System.out.print("What is your name? ");
+    var name = scanner.nextLine();
+    System.out.println("Hello, " + name + ",nice to meet you!");
+  
+
+   
+  }
+}
+
+
+
+
+
